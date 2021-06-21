0d44d7ee1f4a41adcf2d5056cd56ba5dfe59665b
15980904537 <2565051131@qq.com>
2021-06-21 11:24:04 +0800

    创建cascader组件--解决computed缓存的问题

--- src/packages/cascaderItem.vue
+++ src/packages/cascaderItem.vue
@@ -54,15 +54,26 @@ export default {
   },
 
   components: {},
-
+  created() {
+    console.log(this.item);
+  },
   computed: {
     rightItem() {
-      let currentSelected = this.selected[this.level];
-      if (currentSelected && currentSelected.children) {
-        return currentSelected.children;
-      } else {
-        return null;
+      if (this.selected[this.level]) {
+        let item = this.item.filter(
+          (item) => item.name === this.selected[this.level].name
+        );
+        if (item && item[0].children && item[0].children.length > 0) {
+          return item[0].children;
+        }
       }
+
+      // let currentSelected = this.selected[this.level];
+      // if (currentSelected && currentSelected.children) {
+      //   return currentSelected.children;
+      // } else {
+      //   return null;
+      // }
     },
   },
 
